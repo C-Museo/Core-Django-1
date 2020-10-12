ebd4d474098ff9ab81e6231cfbc35b051637ffc6
comfort museo <you@example.com>
2020-10-12 18:11:57 +0300

    add app url configurations

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 def welcome(request):
-        title='Welcome to my gallery'
-    return render(request, 'gallery/home.html'
+    title='Welcome to my gallery'
+    return render( request, 'gallery/home.html' ,{'title': title} )
         
