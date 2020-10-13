58b9e36ae7ac2ee8b691d0ce8fa9631b4f5777d2
comfort museo <you@example.com>
2020-10-13 12:00:19 +0300

    add image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+#Image
+#Image Name.
+#Image Description.
+#Image Location Foreign Key.
+#Image category Foreign Key.
+
+class Image(models.Model):
+    image_name=models.CharField(max_length =30)
+    image_decsription=models.CharField(max_length =30)
+    image_location=models.CharField(max_length =30)
+    image_category=models.CharField(max_length =30)
